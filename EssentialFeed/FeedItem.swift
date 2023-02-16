ab2cd78bbac9bb6cf7064d89f143af3adc8fca02
Thu Do <dothu.khtn@gmail.com>
2023-02-16 21:08:53 +0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Thu Do on 16/02/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
