1dcd4cde03dd8a71f8a5d1e9c915a949343815e5
Thu Do <dothu.khtn@gmail.com>
2023-04-27 15:03:40 +0700

    make comment clearer

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -18,7 +18,7 @@ public final class RemoteFeedLoader: FeedLoader {
     
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
-            guard self != nil else { return } // this is to avoid static func "map" being called even when RemoteFeedLoader has been deallocated.
+            guard self != nil else { return } // this is to avoid static func "map" being called even when RemoteFeedLoader object has been deallocated.
             
             switch result {
             case let .success(data, response):
