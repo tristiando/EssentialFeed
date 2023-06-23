ef756f691b158ad242101b55ee87a6711c01e02b
Thu Do <dothu.khtn@gmail.com>
2023-06-23 18:30:40 +0700

    `CoreDataFeedStore.retrieve()` delivers empty on empty cache

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Thu Do on 23/06/2023.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+}
---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,58 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Thu Do on 23/06/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+    }
+    
+    func test_storeSideEffects_runSerially() {
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+
+}
