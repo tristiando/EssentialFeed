61aec4d6de28503db72a7eed9dfbddaafbe76ff2
Thu Do <dothu.khtn@gmail.com>
2023-07-12 15:11:01 +0700

    Refactor `CachedFeed` type from enum to type since we can represent the absence of a value with an `Optional`

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -21,11 +21,11 @@ public final class CoreDataFeedStore: FeedStore {
         perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
-                    completion(.success(.found(
+                    completion(.success(CachedFeed(
                         feed: cache.localFeed,
                         timestamp: cache.timestamp)))
                 } else {
-                    completion(.success(.empty))
+                    completion(.success(.none))
                 }
             } catch {
                 completion(.failure(error))
