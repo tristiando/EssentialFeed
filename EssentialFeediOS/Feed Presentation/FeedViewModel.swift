48f834088c548422a779b655704c5233f772434e
Thu Do <dothu.khtn@gmail.com>
2023-08-31 14:47:24 +0700

    Move MVVM ViewModels to a new Feed Presentation group to clarify the architectural separation between Presentation and framework-specific UI

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Thu Do on 20/08/2023.
+//
+
+import EssentialFeed
+
+final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
+     
+    func loadFeed() {
+        onLoadingStateChange?(true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onFeedLoad?(feed)
+            }
+            
+            self?.onLoadingStateChange?(false)
+        }
+    }
+}
